7bd99b24a569839c3190fab228e2a11367e673d7
Jens Brandenburg <jens@jbrandenburg.de>
2026-03-09 18:23:00 +0100

    switched up file structure a little, incrementing versions in config file, (re-)create source mod

--- tools/main.bat
+++ tools/main.bat
@@ -4,7 +4,7 @@ setlocal EnableDelayedExpansion
 rem change working directory to directory where script resides
 cd /d "%~dp0"
 
-set DEBUG_MODE=False
+set DEBUG_MODE=True
 
 rem https://stackoverflow.com/a/31721353 + usebackq for quotes in path, "'s around variables to close same, %~dp0 to execute from script's working directory
 for /f "usebackq tokens=1,2 delims==" %%G in ("%~dp0..\config.env") do set "%%G=%%H"
@@ -14,58 +14,75 @@ if "%NDF_PARSE_SOURCE_MOD_NAME%"=="" echo *** Error - NDF_PARSE_SOURCE_MOD_NAME 
 if "%USER_MOD_CONFIG_FOLDER%"=="" echo *** Error - USER_MOD_CONFIG_FOLDER is not defined in config.env! & goto end
 
 echo *** Loaded config.env:
-echo WARNO Mods folder is at %WARNO_MODS_FOLDER%
-echo Source Mod name is %NDF_PARSE_SOURCE_MOD_NAME%
-echo Mod Config folder is %USER_MOD_CONFIG_FOLDER%
+echo - WARNO Mods folder: %WARNO_MODS_FOLDER%
+echo - Source mod name: %NDF_PARSE_SOURCE_MOD_NAME%
+echo - Mod Config folder: %USER_MOD_CONFIG_FOLDER%
+echo.
 
 :main
 call :menu
 
 if %CHOICE% equ 1 (
+  pushd %WARNO_MODS_FOLDER%
+  if exist %WARNO_MODS_FOLDER%\%NDF_PARSE_SOURCE_MOD_NAME%\ (
+    echo %NDF_PARSE_SOURCE_MOD_NAME% exists, removing ...
+    rd /s /q %WARNO_MODS_FOLDER%\%NDF_PARSE_SOURCE_MOD_NAME%\
+  )
+  
+  call CreateNewMod.bat "%NDF_PARSE_SOURCE_MOD_NAME%"
+  popd
+  
+  goto main
+)
+
+if %CHOICE% equ 2 (
   set MOD_NAME=DoubleSupply
   call process_mod_info.bat "!MOD_NAME!" "%WARNO_MODS_FOLDER%" "%NDF_PARSE_SOURCE_MOD_NAME%" "%USER_MOD_CONFIG_FOLDER%" %DEBUG_MODE%
   goto main
 )
 
-if %CHOICE% equ 2 (
+if %CHOICE% equ 3 (
   set MOD_NAME=TripleSupply
   call process_mod_info.bat "!MOD_NAME!" "%WARNO_MODS_FOLDER%" "%NDF_PARSE_SOURCE_MOD_NAME%" "%USER_MOD_CONFIG_FOLDER%" %DEBUG_MODE%
   goto main
 )
 
-if %CHOICE% equ 3 (
+if %CHOICE% equ 4 (
   set MOD_NAME=YIMBY
   call process_mod_info.bat "!MOD_NAME!" "%WARNO_MODS_FOLDER%" "%NDF_PARSE_SOURCE_MOD_NAME%" "%USER_MOD_CONFIG_FOLDER%" %DEBUG_MODE%
   goto main
 )
 
-if %CHOICE% equ 4 (
+if %CHOICE% equ 5 (
   set MOD_NAME=Debug
   call process_mod_info.bat "!MOD_NAME!" "%WARNO_MODS_FOLDER%" "%NDF_PARSE_SOURCE_MOD_NAME%" "%USER_MOD_CONFIG_FOLDER%" %DEBUG_MODE%
   goto main
 )
 
-if %CHOICE% equ 5 call :toggledebug & goto main
-if %CHOICE% equ 6 goto end
+if %CHOICE% equ 6 call :toggledebug & goto main
+if %CHOICE% equ 7 goto end
 
 :menu
-echo.
-echo Select an option:
+echo MAIN MENU
 echo =================
-echo 1 - Update DoubleSupply
-echo 2 - Update TripleSupply
-echo 3 - Update YIMBY
-echo 4 - Test
+echo Select an option:
+echo.
+echo 1 - Generate/Refresh "pristine" Source Mod - Do this before choosing any other option! [VERSION NUMBER HERE]
+echo -----------------
+echo 2 - Generate/Update DoubleSupply
+echo 3 - Generate/Update TripleSupply
+echo 4 - Generate/Update YIMBY
+echo 5 - Test
 echo -----------------
 echo D - Toggle Debug Mode (currently %DEBUG_MODE%)
 echo -----------------
 echo X - Exit
 echo.
 
-choice /C 1234DX /M "Select an option "
+choice /C 12345DX /M "Select an option "
 set CHOICE=%ERRORLEVEL%
 echo.
-exit /b %CHOICE%
+exit /b
 
 :end
 echo *** Exiting
@@ -79,4 +96,6 @@ if %DEBUG_MODE%==True (
   set DEBUG_MODE=True
 )
 echo *** Debug set to %DEBUG_MODE%
+echo.
+
 exit /b
--- tools/process_mod_info.bat
+++ tools/process_mod_info.bat
@@ -8,6 +8,9 @@ set NDF_PARSE_SOURCE_MOD_NAME=%~3
 set USER_MOD_CONFIG_FOLDER=%~4
 set DEBUG_MODE=%~5
 
+set "CONFIG_FILE=%USER_MOD_CONFIG_FOLDER%\%MOD_NAME%\Config.ini"
+set "TEMP_CONFIG=%CONFIG_FILE%.tmp"
+
 echo *** Debugging output:
 echo MOD_NAME %MOD_NAME%
 echo WARNO_MODS_FOLDER %WARNO_MODS_FOLDER%
@@ -16,7 +19,7 @@ echo USER_MOD_CONFIG_FOLDER %USER_MOD_CONFIG_FOLDER%
 echo DEBUG_MODE %DEBUG_MODE%
 echo.
 
-for /f "usebackq tokens=1,2 eol=; delims==" %%G in ("%USER_MOD_CONFIG_FOLDER%\%MOD_NAME%\Config.ini") do (
+for /f "usebackq tokens=1,2 eol=; delims==" %%G in ("%CONFIG_FILE%") do (
   rem clear from previous loop iterations:
   set VALUE=
   
@@ -26,27 +29,63 @@ for /f "usebackq tokens=1,2 eol=; delims==" %%G in ("%USER_MOD_CONFIG_FOLDER%\%M
 )
 
 echo *** Mod version: %Version%
-echo *** Gameversion mod was generated for: %ModGenVersion%
+echo *** Gameversion this mod was generated for: %ModGenVersion%
 
-set PYTHON_SCRIPT_LOCATION="%~dp0..\parse\%MOD_NAME%"
+set PYTHON_SCRIPT_LOCATION="%~dp0..\mods\%MOD_NAME%"
 
 poetry run python %PYTHON_SCRIPT_LOCATION%\modify_values.py "%WARNO_MODS_FOLDER%" "%NDF_PARSE_SOURCE_MOD_NAME%" %DEBUG_MODE%
 set RETURN=%ERRORLEVEL%
 
 if %RETURN% equ 0 (
-  echo.
-  echo *** Successfully edited mod files - would you like to update your mod?
-  
-  set "MY_MOD_FOLDER=%WARNO_MODS_FOLDER%\%MOD_NAME%"
-  
-  rem pushd and popd set current directory to MY_MOD_FOLDER and back, respectively
-  pushd !MY_MOD_FOLDER!
-  call UpdateMod.bat
-  popd
-  
+  if "%DEBUG_MODE%"=="True" (
+    echo.
+    echo ndf_parse executed successfully, exiting since DEBUG_MODE == True
+    echo.
+  ) else (
+    echo.
+    echo *** ^(Re-^)Generating Mod
+
+    set "MY_MOD_FOLDER=%WARNO_MODS_FOLDER%\%MOD_NAME%"
+    pushd !MY_MOD_FOLDER!
+    call GenerateMod.bat
+    call :update_version
+    
+    choice /C YN /M "*** Successfully edited mod files - would you like to upload your mod? "
+    echo.
+    set CHOICE=!ERRORLEVEL!
+
+    if !CHOICE! equ 1 (
+      echo *** Uploading !MY_MOD_FOLDER!
+      echo.
+      call UploadMod.bat
+    )
+
+    popd
+  )
+
   exit /b
 )
 
 echo.
 echo Errorlevel "%RETURN%"
+exit /b
+
+
+:update_version
+set /a Version+=1
+echo *** New Mod Version: %Version%
+
+(
+  for /f "usebackq delims=" %%L in ("%CONFIG_FILE%") do (
+      set "line=%%L"
+      
+      if /i "!line:~0,7!"=="Version" (
+          echo Version = !Version!
+      ) else (
+          echo !line!
+      )
+  )
+) > "%TEMP_CONFIG%"
+
+move /y "%TEMP_CONFIG%" "%CONFIG_FILE%" >nul
 exit /b
