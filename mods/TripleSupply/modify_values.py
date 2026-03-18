10ed8dd99cd61047ccd6fd3791ac9df03434e73d
Jens Brandenburg <jens@jbrandenburg.de>
2026-03-18 03:08:51 +0100

    slightly more verbose code

--- mods/TripleSupply/modify_values.py
+++ mods/TripleSupply/modify_values.py
@@ -12,27 +12,33 @@ try:
     mod.check_if_src_is_newer()
 
     print("\n+ ndf_parse: Modifying Buildings")
-    with mod.edit(r"GameData/Generated/Gameplay/Gfx/BuildingDescriptors.ndf", not debug_mode) as source:
-        for obj_row in source:
-            module_descriptors_row = obj_row.value.by_member("ModulesDescriptors")
-            for list_row in module_descriptors_row.value.match_pattern("TSupplyModuleDescriptor()"):
-                supply_capacity = list_row.value.by_member("SupplyCapacity")
-                original_value = float(supply_capacity.value)
-                modified_value = original_value * supply_multiplier
-                supply_capacity.value = modified_value
-                print(f"+ ndf_parse: {obj_row.namespace} - SupplyCapacity {original_value} --> {modified_value}")
+    with mod.edit(r"GameData/Generated/Gameplay/Gfx/BuildingDescriptors.ndf", not debug_mode) as root_List:
+        for t_entity_descriptor_ListRow in root_List:
+            t_entity_descriptor_Object = t_entity_descriptor_ListRow.value
+            modules_descriptors_List = t_entity_descriptor_Object.by_member("ModulesDescriptors")
+        
+            for modules_descriptors_ListRow in modules_descriptors_List.value.match_pattern("TSupplyModuleDescriptor()"):
+                t_supply_module_descriptor_Object = modules_descriptors_ListRow.value
+                supply_capacity_MemberRow = t_supply_module_descriptor_Object.by_member("SupplyCapacity")
+                original_supply_capacity_value = float(supply_capacity_MemberRow.value)
+                new_supply_capacity_value = original_supply_capacity_value * supply_multiplier
+                supply_capacity_MemberRow.edit(value=str(new_supply_capacity_value))
+                print(f"+ ndf_parse: {t_entity_descriptor_ListRow.namespace} - SupplyCapacity {original_supply_capacity_value} --> {new_supply_capacity_value}")
     print("+ ndf_parse: Supply Buildings DONE!\n")
 
     print("+ ndf_parse: Modifying Units")
-    with mod.edit(r"GameData/Generated/Gameplay/Gfx/UniteDescriptor.ndf", not debug_mode) as source:
-        for obj_row in source:
-            module_descriptors_row = obj_row.value.by_member("ModulesDescriptors")
-            for list_row in module_descriptors_row.value.match_pattern("TSupplyModuleDescriptor()"):
-                supply_capacity = list_row.value.by_member("SupplyCapacity")
-                original_value = float(supply_capacity.value)
-                modified_value = original_value * supply_multiplier
-                supply_capacity.value = modified_value
-                print(f"+ ndf_parse: {obj_row.namespace} - SupplyCapacity {original_value} --> {modified_value}")
+    with mod.edit(r"GameData/Generated/Gameplay/Gfx/UniteDescriptor.ndf", not debug_mode) as root_List:
+        for t_entity_descriptor_ListRow in root_List:
+            t_entity_descriptor_Object = t_entity_descriptor_ListRow.value
+            modules_descriptors_List = t_entity_descriptor_Object.by_member("ModulesDescriptors")
+        
+            for modules_descriptors_ListRow in modules_descriptors_List.value.match_pattern("TSupplyModuleDescriptor()"):
+                t_supply_module_descriptor_Object = modules_descriptors_ListRow.value
+                supply_capacity_MemberRow = t_supply_module_descriptor_Object.by_member("SupplyCapacity")
+                original_supply_capacity_value = float(supply_capacity_MemberRow.value)
+                new_supply_capacity_value = original_supply_capacity_value * supply_multiplier
+                supply_capacity_MemberRow.edit(value=str(new_supply_capacity_value))
+                print(f"+ ndf_parse: {t_entity_descriptor_ListRow.namespace} - SupplyCapacity {original_supply_capacity_value} --> {new_supply_capacity_value}")
     print("+ ndf_parse: Supply Units DONE!\n")
     sys.exit(0)
 except Exception as err:
